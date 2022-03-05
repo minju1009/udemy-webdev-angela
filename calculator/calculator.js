d88ffd1b42a4e02889952b4a607763945201d01f
Minju <minju1009@gmail.com>
2022-03-05 21:40:55 +0900

    learning how to run server with nodejs, expressjs, nodemon, npm

---
+++ calculator/calculator.js
@@ -0,0 +1,28 @@
+const express =require('express');
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+
+const num1 = Number(req.body.num1);
+const num2 = Number(req.body.num2);
+
+const result = num1 + num2;
+
+res.send("The result of the calculation is " + result);
+
+
+})
+
+
+
+
+
+app.listen(port, () =>
+console.log("listening to the port..."));
