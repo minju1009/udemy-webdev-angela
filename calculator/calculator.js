a706d45420f78e1b5fdf1b708e1e55f5eef9cd47
Minju <minju1009@gmail.com>
2022-03-06 23:57:17 +0900

    create calculator that runs on the backend with jsnode

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -8,18 +8,24 @@ app.get('/', (req, res) => {
    res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req,res){
-
-const num1 = Number(req.body.num1);
-const num2 = Number(req.body.num2);
-
-const result = num1 + num2;
-
-res.send("The result of the calculation is " + result);
-
-
+app.get('/bmiCalculator', (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html")
 })
 
+
+app.post("/", function(req,res){
+const num1 = Number(req.body.num1);
+const num2 = Number(req.body.num2);
+const result = num1 + num2;
+res.send("The result of the calculation is " + result);
+})
+
+app.post("/bmiCalculator", function(req,res){
+  const weight = Number(req.body.weight);
+  const height = Number(req.body.height);
+  const result = weight/(height*height);
+  res.send("Your BMI is " + Math.ceil(result) );
+})
 
 
 
